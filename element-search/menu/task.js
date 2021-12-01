2a76cdd7b6cc37e56c2b1354f419f8f14f87caeb
Valentin Korneev <v.korneef@gmail.com>
2021-12-01 22:00:46 +0500

    hm v3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,16 +3,18 @@ const menuLinkCount = menuLink.length;
 
 for (let i = 0; i < menuLinkCount; i++) {
     menuLink[i].onclick = function() {
-        if (this.parentElement.querySelector('ul.menu_sub') != null) {
-            
-            let activeMenu = document.querySelector('ul.menu_active');
-            if (activeMenu != null) {
-                activeMenu.className = 'menu menu_sub';
-            //  activeMenu.classname = activeMenu.classname.replace(' menu_active', '');
+        let menu = this.parentElement.querySelector('ul.menu_sub');    
+        menu.className.includes('menu_active') ? menu.className = menu.className.replace(' menu_active', '') : menu.className += ' menu_active';
+        
+        let activeMenu = document.querySelectorAll('ul.menu_active');
+        
+        if (activeMenu.length > 1) {
+            for (let i = 0; i < activeMenu.length; i++) {
+                activeMenu[i].className = activeMenu[i].className.replace(' menu_active', '');
             };
-
-            this.parentElement.querySelector('ul.menu_sub').className += ' menu_active'
-            return false
+            menu.className += ' menu_active';
+        }
+        
+        return false
         };
     };
-};
