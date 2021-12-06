92c05582f7add3964c914d7b0faf28859af16178
Valentin Korneev <v.korneef@gmail.com>
2021-12-06 21:51:15 +0500

    hm v4

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,25 @@
-let currentSlide = 0;
-let slides = document.querySelectorAll('.slider__item');
+const slides = Array.from(document.querySelectorAll('.slider__item'));
 
 document.querySelector('.slider__arrow_prev').onclick = function() {
-    console.log('hi')
-    slides[currentSlide].className = 'slider__item';
-    if (currentSlide <= 0) {
+    let currentSlide = slides.findIndex((item, idx) => item.className.includes('slider__item_active') ? true : false);
+    slides[currentSlide].className = 'slider__item'
+    if (currentSlide === 0) {
         currentSlide = slides.length - 1;
     } else {
-        currentSlide = currentSlide - 1;
+        currentSlide -= 1;
     };
-    slides[currentSlide].className += ' slider__item_active';
+
+    slides[currentSlide].className += ' slider__item_active'
 };
 
 document.querySelector('.slider__arrow_next').onclick = function() {
-    slides[currentSlide].className = 'slider__item';
-    if (currentSlide >= slides.length - 1) {
+    let currentSlide = slides.findIndex((item, idx) => item.className.includes('slider__item_active') ? true : false);
+    slides[currentSlide].className = 'slider__item'
+    if (currentSlide === (slides.length - 1)) {
         currentSlide = 0;
     } else {
-        currentSlide = currentSlide + 1;
+        currentSlide += 1;
     };
-    slides[currentSlide].className += ' slider__item_active';
-};
 
+    slides[currentSlide].className += ' slider__item_active'
+};
