aa3e81c3c25b8880eb9317847b4ffeb3433f6679
Valentin Korneev <v.korneef@gmail.com>
2021-12-20 23:01:27 +0500

    hm v1

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,15 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    document.addEventListener('keydown', (event) => {
+      let key = 0;
+      key = event.key.toLowerCase();
+      if (key === this.currentSymbol.textContent) {
+        this.success();
+      } else {
+        this.fail();
+      };
+    });
   }
 
   success() {
@@ -56,18 +58,18 @@ class Game {
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -77,7 +79,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+let tabNavigationElements = Array.from(document.querySelectorAll('.tab'));
+
+for (let i = 0; i < tabNavigationElements.length; i++) {
+    tabNavigationElements[i].addEventListener('click', eventToClick);
+}
+
+function eventToClick () {
+    if (!this.className.includes('tab_active')) {
+        let oldActiveTabItem = tabNavigationElements.findIndex((item) => item.className.includes('tab_active'));
+        tabNavigationElements[oldActiveTabItem].className = 'tab';
+        this.className = 'tab tab_active';
+
+        document.querySelectorAll('.tab__content')[oldActiveTabItem].className = 'tab__content';
+        document.querySelectorAll('.tab__content')[tabNavigationElements.indexOf(this)].className = 'tab__content tab__content_active';
+    }
+}
